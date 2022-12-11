a1ae7c9e3bba8866c29292ffacef7094a4c2a734
wbonna352 <wojciechbonna@gmail.com>
2022-12-11 10:13:00 +0100

    add day_08 part two

--- day_08/main.py
+++ day_08/main.py
@@ -15,17 +15,42 @@ def tree_visible(r: int, c: int, array: np.array) -> bool:
     return np.any([left, right, top, bottom])
 
 
+def scenic_score(r: int, c: int, array: np.array) -> int:
+    v = array[r, c]
+
+    def count_trees(value: int, vector: np.array) -> int:
+        trees = 0
+        for i in vector:
+            trees += 1
+            if i >= value:
+                break
+        return trees
+
+    vectors = {
+        'left': array[r, :c][::-1],
+        'right': array[r, (c+1):],
+        'top': array[:r, c][::-1],
+        'bottom': array[(r+1):, c]
+    }
+
+    return np.prod([count_trees(v, vector) for vector in vectors.values()])
+
+
 def main() -> None:
     with open('input.txt') as f:
         input_data = f.read()
     trees = digit_array_from_str(input_data)
 
-    value = 0
+    visible_count = 0
+    max_scenic_score = 0
     for r in range(trees.shape[0]):
         for c in range(trees.shape[1]):
-            value += int(tree_visible(r, c, trees))
+            visible_count += int(tree_visible(r, c, trees))
+            if (tree_scenic_score := scenic_score(r, c, trees)) > max_scenic_score:
+                max_scenic_score = tree_scenic_score
 
-    print(value)
+    print('Part one: ', visible_count)
+    print('Part two: ', max_scenic_score)
 
 
 if __name__ == '__main__':
